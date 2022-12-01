960b2dbfe37df78900c447dc664151d192b7ad82
Keanu <112871956+Keanu-Tjung@users.noreply.github.com>
2022-12-01 10:11:50 +0100

    opdracht_3.3

---
+++ opdracht_3.3/app.js
@@ -0,0 +1,14 @@
+// opdracht 1
+// let getal = 10;
+// 
+// for(let i = 1; i <= 10; i++){
+// console.log(i + " x " + getal + "=" + (getal * i))
+// }
+
+// opdracht 2
+// function tafel_van(getal){
+// for(let i = 1; i <= 10; i++){
+// console.log(i + " x " + getal + "=" + (getal * i))
+// }}
+// 
+// tafel_van(10)
